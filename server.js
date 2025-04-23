6dc871767b19469ae2a47222d954d6bd934afaa8
vulcan-pb <singh.darsh03@gmail.com>
2025-04-23 17:58:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_KEY);
 
 app.post('/login', async (req, res) => {
   const { email, password } = req.body;
